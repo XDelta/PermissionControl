e3d8d2c40fdc04113e8b6789eace511789d4af9f
Delta <delta@deltawolf.net>
2026-03-03 00:52:34 -0800

    Move properties

--- PermissionControl/Properties/AssemblyInfo.cs
+++ PermissionControl/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: AssemblyProduct("PermissionControl")]
 [assembly: AssemblyDescription("Allows more fine control over permission overrides")]
 [assembly: AssemblyCompany("Delta")]
-[assembly: AssemblyCopyright("Copyright © 2025 Delta")]
+[assembly: AssemblyCopyright("Copyright © 2026 Delta")]
 [assembly: AssemblyVersion(PermissionControl.PermissionControl.VERSION_CONSTANT)]
 [assembly: AssemblyFileVersion(PermissionControl.PermissionControl.VERSION_CONSTANT)]
